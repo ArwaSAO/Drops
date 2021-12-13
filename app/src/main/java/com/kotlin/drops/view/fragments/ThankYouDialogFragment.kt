0f77f98073c78349f8b5b4b35bc05ba171f6a6fb
= <=>
2021-12-13 10:12:00 +0300

    finalize XML design

---
+++ app/src/main/java/com/kotlin/drops/view/fragments/ThankYouDialogFragment.kt
@@ -0,0 +1,23 @@
+package com.kotlin.drops.view.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.kotlin.drops.R
+
+
+class ThankYouDialogFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_thank_you_dialog, container, false)
+    }
+
+
+}
