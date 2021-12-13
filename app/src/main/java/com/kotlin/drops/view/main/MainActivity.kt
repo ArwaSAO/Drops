9a2a0ba7e85f9e86687b6508d56632d79666cf39
= <=>
2021-12-13 16:13:00 +0300

    MainActivity.kt,

--- app/src/main/java/com/kotlin/drops/view/main/MainActivity.kt
+++ app/src/main/java/com/kotlin/drops/view/main/MainActivity.kt
@@ -2,12 +2,47 @@ package com.kotlin.drops.view.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.kotlin.drops.R
+import com.kotlin.drops.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+        // using binding -->> no need for findViewById method
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView2) as NavHostFragment
+        navController = navHostFragment.navController
+
+        setupActionBarWithNavController(navController)
+        // to link the nav bottom with nav host
+        NavigationUI.setupWithNavController(binding.bottomNavigationView, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+
+    }
+
+
+
+
 
 
     }
