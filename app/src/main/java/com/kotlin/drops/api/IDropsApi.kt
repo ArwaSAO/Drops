60c502c875f20d30214c27579d5bba6b69d92455
= <=>
2021-12-14 11:56:45 +0300

    try to solve XML erroprs

--- app/src/main/java/com/kotlin/drops/api/IDropsApi.kt
+++ app/src/main/java/com/kotlin/drops/api/IDropsApi.kt
@@ -1,5 +1,14 @@
 package com.kotlin.drops.api
 
+import com.kotlin.drops.model.PatientInfoDataModelItem
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Header
+
 interface IDropsApi {
 
+    @GET("/Common/Product/GetProductsForSell?page=0")
+    suspend fun getProducts(
+        @Header("Authorization") token: String
+    ) : Response<PatientInfoDataModelItem>
 }
