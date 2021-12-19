397f7782e24c20c1e5661ebc973e024b188fa211
= <=>
2021-12-19 11:59:32 +0300

    Working on Api's

--- app/src/main/java/com/kotlin/drops/api/IDropsApi.kt
+++ app/src/main/java/com/kotlin/drops/api/IDropsApi.kt
@@ -4,7 +4,10 @@ import com.kotlin.drops.model.Donataitons
 import com.kotlin.drops.model.DonorInfo
 import com.kotlin.drops.model.PatientInfo
 import retrofit2.Response
+import retrofit2.http.DELETE
 import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.PUT
 
 
 interface IDropsApi {
@@ -15,21 +18,55 @@ interface IDropsApi {
     @GET("/PatientInfo")
     suspend fun getPatientInfo(): Response<PatientInfo>
 
+    @GET("/PatientInfo/:id")
+    suspend fun getPatientInfoId(): Response<PatientInfo>
 
-    //=================  Donations Info =========================================================//
+    @POST("/PatientInfo")
+    suspend fun addPatientInfo(): Response<PatientInfo>
 
+    @PUT("/PatientInfo/:id")
+    suspend fun upDatePatientInfo(): Response<PatientInfo>
+
+    @DELETE("/PatientInfo/:id")
+    suspend fun deletePatientInfo(): Response<PatientInfo>
+
+
+    //================= Donor Info =========================================================//
+
+
+    @GET("/donorInfo")
+    suspend fun getDonorInfo(): Response<DonorInfo>
+
+    @GET("/donorInfo/:id")
+    suspend fun getDonorsId(): Response<DonorInfo>
+
+    @POST("/donorInfo")
+    suspend fun addDonorInfo(): Response<DonorInfo>
+
+    @PUT("/donorInfo/:id")
+    suspend fun updateDonorInfo(): Response<DonorInfo>
+
+    @DELETE("/donorInfo/:id")
+    suspend fun deleteDonerInfo(): Response<DonorInfo>
+
+
+    //==================  Donation Info  ============================================================//
 
 
     @GET("/donations")
     suspend fun getDonationsInfo(): Response<Donataitons>
 
+    @GET("/donations/:id")
+    suspend fun getDonationId(): Response<Donataitons>
 
+    @POST("/donations")
+    suspend fun addDonationsId(): Response<Donataitons>
 
-    //==================  DonorInfo  ============================================================//
+    @PUT("/donations/:id")
+    suspend fun updateDonations(): Response<Donataitons>
 
-
-    @GET("/donorInfo")
-    suspend fun getDonorInfo(): Response<DonorInfo>
+    @DELETE("/donations/:id")
+    suspend fun deleteDonations(): Response<Donataitons>
 
 
 }
