97fc597880b62acfd29c9e3ab33af7022feded52
= <=>
2021-12-28 13:28:23 +0300

    update the IDropsApi.kt and create BokkingViewModel.kt, PatientInfoViewModel.kt, and complete working on all fragments

--- app/src/main/java/com/kotlin/drops/view/main/SplashActivity.kt
+++ app/src/main/java/com/kotlin/drops/view/main/SplashActivity.kt
@@ -8,26 +8,36 @@ import android.os.Bundle
 import android.os.CountDownTimer
 import android.util.Log
 import com.kotlin.drops.R
+import com.kotlin.drops.databinding.ActivitySplashBinding
+import com.kotlin.drops.reposetories.ApiServiceRepository
 import com.kotlin.drops.view.identity.LoginActivity
 
- lateinit var sharedPref: SharedPreferences
- lateinit var sharedPrefEditor: SharedPreferences.Editor
+lateinit var sharedPref: SharedPreferences
+lateinit var sharedPrefEditor: SharedPreferences.Editor
 private const val TAG = "SplashActivity"
+
 class SplashActivity : AppCompatActivity() {
+
+
+    private lateinit var binding: ActivitySplashBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         supportActionBar?.hide()
+        ApiServiceRepository.init(this)
 
+        //set time for splash
+        val intent = Intent(this, LoginActivity::class.java)
 
-// //set time for splash
-        val intent= Intent(this, LoginActivity::class.java)
-
-
-        object : CountDownTimer(2000,1000){
+        object : CountDownTimer(2000, 1000) {
             override fun onTick(p0: Long) {
             }
+
             override fun onFinish() {
                 Log.d(TAG, "finish")
                 startActivity(intent)
